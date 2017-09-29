4ebb4fef8fae73a879b927e94bf33d11a323973e
arvindtota <31419033+arvindtota@users.noreply.github.com>
2017-09-28 23:52:04 -0500

    ASE 5

---
+++ Source/app/src/main/java/com/clarifai/android/starter/api/v2/MainActivity.java
@@ -0,0 +1,13 @@
+package arvind.clarifai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
